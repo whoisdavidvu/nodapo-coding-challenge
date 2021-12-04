e7d9a2f4a05bdf6f0831f77484dc615b49a5ef9c
David Vu <davidvu396@googlemail.com>
2021-12-04 18:24:52 +0100

    added first book test

---
+++ bookshop/src/test/java/com/whoisdavidvu/bookshop/BookTest.java
@@ -0,0 +1,47 @@
+package com.whoisdavidvu.bookshop;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BookTest {
+
+    // tests if the book has empty title
+    @Test
+    public void creatingBook_throwsIAEX_ifTitleEmpty() {
+        assertThrows(IllegalArgumentException.class, ()->{
+            Book narnia = new Book("", "19.99", 365, Genres.Fantasy);
+        });
+    }
+
+    // tests if the book price is negative
+    @Test
+    public void creatingBook_throwsIAEX_ifPriceNegative() {
+        assertThrows(IllegalArgumentException.class, ()->{
+            Book narnia = new Book("Narnia", "-19.99", 365, Genres.Fantasy);
+        });
+    }
+
+    // tests if the book price has too many digits after decimal point
+    @Test
+    public void creatingBook_throwsIAEX_ifPriceHasTooManyDigits() {
+        assertThrows(IllegalArgumentException.class, ()->{
+            Book narnia = new Book("Narnia", "19.999", 365, Genres.Fantasy);
+        });
+    }
+
+    // tests if the book has 0 pages or a negative page count
+    @Test
+    public void creatingBook_throwsIAEX_ifPageCountInvalid() {
+        assertThrows(IllegalArgumentException.class, ()->{
+            Book narnia = new Book("Narnia", "19.99", 0, Genres.Fantasy);
+        });
+        assertThrows(IllegalArgumentException.class, ()->{
+            Book narnia = new Book("Narnia", "19.99", -1, Genres.Fantasy);
+        });
+    }
+
+}
