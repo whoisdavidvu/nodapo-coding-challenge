fcc392db035c85a991b9881310fdd584e6fce183
David Vu <davidvu396@googlemail.com>
2021-12-04 14:29:32 +0100

    changed genres

--- bookshop/src/main/java/com/whoisdavidvu/bookshop/Genres.java
+++ bookshop/src/main/java/com/whoisdavidvu/bookshop/Genres.java
@@ -1,11 +1,8 @@
 package com.whoisdavidvu.bookshop;
 
 public enum Genres {
-    Comedy, 
-    Horror,
-    Action,
-    Romance,
-    Documentary,
-    Thriller,
-    Drama
+    Adventure, 
+    Biography,
+    Comic,
+    Fantasy
 }
