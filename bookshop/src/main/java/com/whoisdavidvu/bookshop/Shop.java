eb2cec75f9ee04b4f5f44e837667c313dafec324
David Vu <davidvu396@googlemail.com>
2021-12-04 18:49:54 +0100

    implemented function to filter genres

--- bookshop/src/main/java/com/whoisdavidvu/bookshop/Shop.java
+++ bookshop/src/main/java/com/whoisdavidvu/bookshop/Shop.java
@@ -2,6 +2,8 @@ package com.whoisdavidvu.bookshop;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Shop {
 
@@ -43,7 +45,7 @@ public class Shop {
 
     // simply adds a book to the inventory arraylist
     public void addBook(Book b) {
-        currentInventory.add(b);
+        this.currentInventory.add(b);
     }
 
     // adds value of sold book into cash and removes book from inventory
@@ -52,6 +54,21 @@ public class Shop {
         this.currentInventory.remove(b);
     }    
 
+    // filters out and returns books with a desired genre
+    public List<Book> filterBookByGenre(String genre) {
+        //ArrayList<Book> filteredList = new ArrayList<Book>();
+        List<Book> filteredList = currentInventory.stream()
+                                .filter(book -> book.getGenre().toString().contains(genre))
+                                .collect(Collectors.toList());
+        return filteredList;
+    }
+
+    // filters out and returns books with a desired genre (this version does not allocate memory)
+    /* public List<Book> filterBookByGenre(String genre) {
+        return currentInventory.stream()
+                                .filter(book -> book.getGenre().toString().contains(genre))
+                                .collect(Collectors.toList());
+    } */
 
 
     // main method for testing basic functionality
@@ -60,21 +77,36 @@ public class Shop {
 
         Book narnia = new Book("Narnia", "19.99", 365, Genres.Fantasy);
         Book gatesBio = new Book("Bill Gates' Biography", "49.99", 512, Genres.Biography);
+        Book jobsBio = new Book("Steve Jobs' Biography", "49.99", 1024, Genres.Biography);
+        Book mobydick = new Book("Moby-Dick", "29.99", 599, Genres.Adventure);
+        Book odyssey = new Book("Odyssey", "19.99", 333, Genres.Adventure);
 
         Customer steve = new Customer("Steve Jobs", "1984");
 
+        
         bookshop.addBook(narnia);
         bookshop.addBook(gatesBio);
+        bookshop.addBook(jobsBio);
+        bookshop.addBook(mobydick);
+        bookshop.addBook(odyssey);
+
+
+        /* // before purchase 
         System.out.println("Sales figure is: " + bookshop.getBalance());
         System.out.println("Inventory is: " + bookshop.getInventory());
         System.out.println();
 
+        // after purchase
         steve.purchasesBook(gatesBio, bookshop);
         System.out.println("Sales figure after sale: " + bookshop.getBalance());
         System.out.println("Inventory after sale: " + bookshop.getInventory());
 
+        // updated customer data
         System.out.println();
-        System.out.println(steve.toString());
+        System.out.println(steve.toString()); */
+
+        System.out.println(bookshop.filterBookByGenre("Adventure"));
+        
     }
     
 }
