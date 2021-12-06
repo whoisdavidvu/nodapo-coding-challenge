4c431fe096362ef3b4bf3e9a6aa16c7cbba79387
David Vu <davidvu396@googlemail.com>
2021-12-06 17:38:47 +0100

    edited shop test for isbn and cleanup

--- bookshop/src/test/java/com/whoisdavidvu/bookshop/ShopTest.java
+++ bookshop/src/test/java/com/whoisdavidvu/bookshop/ShopTest.java
@@ -11,13 +11,34 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ShopTest {
-    
+    /* Shop bookshop = new Shop("Thalia", "1337");
+    Shop secondshop = new Shop("Mayersche", "1024"); */
+
+    String validISBN = "978-3608963762";
+    String invalidISBN = "978-3442267747";
+
+    Book narnia = new Book("Narnia", "19.99", 365, Genres.Fantasy, validISBN);
+    Book gatesBio = new Book("Bill Gates' Biography", "69.99", 512, Genres.Biography, validISBN);
+    Book gatesBioDuplicate = new Book("Bill Gates' Biography", "79.99", 512, Genres.Biography, validISBN);
+    Book jobsBio = new Book("Steve Jobs' Biography", "79.99", 1024, Genres.Biography, validISBN);
+    Book mobydick = new Book("Moby-Dick", "29.99", 599, Genres.Adventure, validISBN);
+    Book odyssey = new Book("Odyssey", "19.99", 333, Genres.Adventure, validISBN);
+    Book lotr = new Book("LOTR", "99.99", 1337, Genres.Fantasy, validISBN);
+
+    Book narniaInv = new Book("Narnia", "19.99", 365, Genres.Fantasy, invalidISBN);
+    Book gatesBioInv = new Book("Bill Gates' Biography", "69.99", 512, Genres.Biography, invalidISBN);
+    Book gatesBioDuplicateInv = new Book("Bill Gates' Biography", "79.99", 512, Genres.Biography, invalidISBN);
+    Book jobsBioInv = new Book("Steve Jobs' Biography", "79.99", 1024, Genres.Biography, invalidISBN);
+    Book mobydickInv = new Book("Moby-Dick", "29.99", 599, Genres.Adventure, invalidISBN);
+    Book odysseyInv = new Book("Odyssey", "19.99", 333, Genres.Adventure, invalidISBN);
+    Book lotrInv = new Book("LOTR", "99.99", 1337, Genres.Fantasy, invalidISBN);
+
+    Customer bill = new Customer("Bill Gates", "2000");
+
     // tests if money is added to cash when book is sold
     @Test
     public void transaction_successful_afterMoneyReceived() {
-        Customer bill = new Customer("Bill Gates", "2000");
         Shop bookshop = new Shop("Thalia", "1337");
-        Book narnia = new Book("Narnia", "19.99", 365, Genres.Fantasy);
         BigDecimal oldBalance = bookshop.getBalance();
         bookshop.addBook(narnia);
         bill.purchasesBook(narnia, bookshop);
@@ -28,9 +49,7 @@ public class ShopTest {
     // correct this test if multiple numbers of one book is available
     @Test
     public void transaction_returnsFalse_ifSoldBookRemoved() {
-        Customer bill = new Customer("Bill Gates", "2000");
         Shop bookshop = new Shop("Thalia", "1337");
-        Book narnia = new Book("Narnia", "19.99", 365, Genres.Fantasy);
         bookshop.addBook(narnia);
         bill.purchasesBook(narnia, bookshop);
         Assertions.assertFalse(bookshop.getInventory().contains(narnia));
@@ -49,13 +68,6 @@ public class ShopTest {
     public void filter_throwsError_ifNoBookFound() {
         assertThrows(Error.class, ()->{
             Shop bookshop = new Shop("Thalia", "1337");
-            
-            Book narnia = new Book("Narnia", "19.99", 365, Genres.Fantasy);
-            Book gatesBio = new Book("Bill Gates' Biography", "49.99", 512, Genres.Biography);
-            Book jobsBio = new Book("Steve Jobs' Biography", "49.99", 1024, Genres.Biography);
-            Book mobydick = new Book("Moby-Dick", "29.99", 599, Genres.Adventure);
-            Book odyssey = new Book("Odyssey", "19.99", 333, Genres.Adventure);
-            
             bookshop.addBook(narnia);
             bookshop.addBook(gatesBio);
             bookshop.addBook(jobsBio);
@@ -70,13 +82,6 @@ public class ShopTest {
     @Test
     public void filter_returnsTrue_ifCorrectBooksFound() {
         Shop bookshop = new Shop("Thalia", "1337");
-            
-        Book narnia = new Book("Narnia", "19.99", 365, Genres.Fantasy);
-        Book gatesBio = new Book("Bill Gates' Biography", "49.99", 512, Genres.Biography);
-        Book jobsBio = new Book("Steve Jobs' Biography", "49.99", 1024, Genres.Biography);
-        Book mobydick = new Book("Moby-Dick", "29.99", 599, Genres.Adventure);
-        Book odyssey = new Book("Odyssey", "19.99", 333, Genres.Adventure);
-            
         bookshop.addBook(narnia);
         bookshop.addBook(gatesBio);
         bookshop.addBook(jobsBio);
@@ -88,6 +93,21 @@ public class ShopTest {
         // function should only find Moby Dick and Odyssey
         Assertions.assertTrue(filteredList.contains(mobydick));
         Assertions.assertTrue(filteredList.contains(odyssey));
+    }
+
+    // tests if the filter-by-genre function makes sure it doesnt find the incorrect books
+    @Test
+    public void filter_returnsFalse_ifIncorrectBooksFound() {
+        Shop bookshop = new Shop("Thalia", "1337");
+        bookshop.addBook(narnia);
+        bookshop.addBook(gatesBio);
+        bookshop.addBook(jobsBio);
+        bookshop.addBook(mobydick);
+        bookshop.addBook(odyssey);
+
+        List<Book> filteredList = bookshop.filterBookByGenre("adventure");
+        
+        // function should not find biographies and fantasy books
         Assertions.assertFalse(filteredList.contains(jobsBio));
         Assertions.assertFalse(filteredList.contains(gatesBio));
         Assertions.assertFalse(filteredList.contains(narnia));
@@ -97,14 +117,6 @@ public class ShopTest {
     @Test
     public void duplicateDelete_returnsTrue_ifShopHasUniqueBooks() {
         Shop bookshop = new Shop("Thalia", "1337");
-
-        Book narnia = new Book("Narnia", "19.99", 365, Genres.Fantasy);
-        Book gatesBio = new Book("Bill Gates' Biography", "49.99", 512, Genres.Biography);
-        Book gatesBioDuplicate = new Book("Bill Gates' Biography", "49.99", 512, Genres.Biography);
-        Book jobsBio = new Book("Steve Jobs' Biography", "49.99", 1024, Genres.Biography);
-        Book mobydick = new Book("Moby-Dick", "29.99", 599, Genres.Adventure);
-        Book odyssey = new Book("Odyssey", "19.99", 333, Genres.Adventure);
-        
         bookshop.addBook(narnia);
         bookshop.addBook(gatesBio);
         bookshop.addBook(gatesBioDuplicate);
@@ -125,18 +137,17 @@ public class ShopTest {
     @Test
     public void matchingBooks_returnsTrue_ifFoundMatchingBooks() {
         Shop bookshop = new Shop("Thalia", "1337");
+        bookshop.addBook(narnia);
+        bookshop.addBook(gatesBio);
+        bookshop.addBook(jobsBio);
+        bookshop.addBook(mobydick);
+        bookshop.addBook(odyssey);
+
         Shop secondshop = new Shop("Mayersche", "1024");
-
-        bookshop.addBook(new Book("Narnia", "19.99", 365, Genres.Fantasy));
-        bookshop.addBook(new Book("Bill Gates' Biography", "49.99", 512, Genres.Biography));
-        bookshop.addBook(new Book("Steve Jobs' Biography", "49.99", 1024, Genres.Biography));
-        bookshop.addBook(new Book("Moby-Dick", "29.99", 599, Genres.Adventure));
-        bookshop.addBook(new Book("Odyssey", "19.99", 333, Genres.Adventure)); 
-
-        secondshop.addBook(new Book("Odyssey", "19.99", 333, Genres.Adventure));
-        secondshop.addBook(new Book("Bill Gates' Biography", "39.99", 512, Genres.Biography));
-        secondshop.addBook(new Book("Steve Jobs' Biography", "29.99", 1024, Genres.Biography));
-        secondshop.addBook(new Book("LOTR", "99.99", 1337, Genres.Fantasy));
+        secondshop.addBook(odyssey);
+        secondshop.addBook(gatesBio);
+        secondshop.addBook(jobsBio);
+        secondshop.addBook(lotr);
         
         Assertions.assertEquals(bookshop.getThisList(secondshop).size(), bookshop.getOtherList(secondshop).size());
 
@@ -159,18 +170,17 @@ public class ShopTest {
     @Test
     public void matchingBooks_returnsFalse_ifNonMatchingBooksInput() {
         Shop bookshop = new Shop("Thalia", "1337");
+        bookshop.addBook(narnia);
+        bookshop.addBook(gatesBio);
+        bookshop.addBook(jobsBio);
+        bookshop.addBook(mobydick);
+        bookshop.addBook(odyssey);
+
         Shop secondshop = new Shop("Mayersche", "1024");
-
-        bookshop.addBook(new Book("Narnia", "19.99", 365, Genres.Fantasy));
-        bookshop.addBook(new Book("Bill Gates' Biography", "49.99", 512, Genres.Biography));
-        bookshop.addBook(new Book("Steve Jobs' Biography", "49.99", 1024, Genres.Biography));
-        bookshop.addBook(new Book("Moby-Dick", "29.99", 599, Genres.Adventure));
-        bookshop.addBook(new Book("Odyssey", "19.99", 333, Genres.Adventure)); 
-
-        secondshop.addBook(new Book("Odyssey", "19.99", 333, Genres.Adventure));
-        secondshop.addBook(new Book("Bill Gates' Biography", "39.99", 512, Genres.Biography));
-        secondshop.addBook(new Book("Steve Jobs' Biography", "29.99", 1024, Genres.Biography));
-        secondshop.addBook(new Book("LOTR", "99.99", 1337, Genres.Fantasy));
+        secondshop.addBook(odyssey);
+        secondshop.addBook(gatesBio);
+        secondshop.addBook(jobsBio);
+        secondshop.addBook(lotr);
         
         Assertions.assertEquals(bookshop.getThisList(secondshop).size(), bookshop.getOtherList(secondshop).size());
 
@@ -187,5 +197,36 @@ public class ShopTest {
             bookshop.getOtherList(secondshop).stream().anyMatch(o -> o.getTitle().equals("LOTR")) ||
             bookshop.getOtherList(secondshop).stream().anyMatch(o -> o.getTitle().equals("Moby-Dick"))
         );
+    }
+
+    // tests if it finds books with invalid ISBN-13
+    @Test
+    public void inventoryCheck_returnsFalse_ifItDoesntFindInvalidBookISBN() {
+        Shop bookshop = new Shop("Thalia", "1337");
+        bookshop.addBook(narnia);
+        bookshop.addBook(gatesBioInv);
+        bookshop.addBook(jobsBioInv);
+        bookshop.addBook(mobydick);
+        bookshop.addBook(odyssey);
+
+        Assertions.assertFalse(
+            bookshop.getInventory().contains(gatesBioInv) ||
+            bookshop.getInventory().contains(jobsBioInv));
+    }
+
+    // tests if it finds books with valid ISBN-13
+    @Test
+    public void inventoryCheck_returnsTrue_ifItFindsValidBookISBN() {
+        Shop bookshop = new Shop("Thalia", "1337");
+        bookshop.addBook(narnia);
+        bookshop.addBook(gatesBioInv);
+        bookshop.addBook(jobsBioInv);
+        bookshop.addBook(mobydick);
+        bookshop.addBook(odyssey);
+
+        Assertions.assertTrue(
+            bookshop.getInventory().contains(narnia) &&
+            bookshop.getInventory().contains(mobydick) &&
+            bookshop.getInventory().contains(odyssey));
     }
 }
